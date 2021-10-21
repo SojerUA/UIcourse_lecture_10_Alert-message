86a9169c2bac9b31563014ad30ad05f8924986cd
Sojer <sojer90@gmail.com>
2021-10-22 01:38:15 +0300

    Work Alert

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+let myName = 'Oleg';
+let myAge = 31;
+let myAnswer = 'Yes, i like Learning Front End.';
+let question = 'My name is ' + myName + '. I am ' + myAge + ' years old.' + ' Do you like learning Front End?   ' + myAnswer;
+//console.log(question)
+message.onclick = function() {
+    alert(question)
+};
