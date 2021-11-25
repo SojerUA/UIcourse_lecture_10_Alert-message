821194726cf9d0e11c42a28a4b7ab6d4eff37265
Sojer <sojer90@gmail.com>
2021-11-25 17:18:53 +0200

    fix elementbyid

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ let myAge = 31;
 let myAnswer = 'Yes, i like Learning Front End.';
 let question = 'My name is ' + myName + '. I am ' + myAge + ' years old.' + ' Do you like learning Front End?   ' + myAnswer;
 //console.log(question)
-message.onclick = function() {
-    alert(question)
+
+let myMessage  = document.getElementById('message');
+
+myMessage.onclick = function() {
+    alert(question);
 };
