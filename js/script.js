653002bc7597971ddbb334a46aef8e2bebd90e17
Oleh Bilyk <sojer90@gmail.com>
2022-07-18 21:06:27 +0300

    fix old work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 
-let myName = 'Oleg';
+/*let myName = 'Oleg';
 let myAge = 31;
 let myAnswer = 'Yes, i like Learning Front End.';
 let question = 'My name is ' + myName + '. I am ' + myAge + ' years old.' + ' Do you like learning Front End?   ' + myAnswer;
@@ -10,4 +10,10 @@ let myMessage  = document.getElementById('message');
 
 myMessage.onclick = function() {
     alert(question);
-};
+};*/
+document.getElementById('message').onclick = function () {
+    let name = "Oleg";
+    let age = 32;
+    let question = "I like learning Front End";
+    return alert(`Hello my name is ${name}, i am ${age}, ${question}`)
+}
